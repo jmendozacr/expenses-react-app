ccae19bb18df792790a77010e82d1ab7a75473f7
Josue Mendoza <josuem@thehangar.cr>
2021-04-27 13:43:23 -0600

    adding theme colors file

---
+++ src/theme/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+    background: '#F9F9F9',
+    primary: '#5B69E2',
+    secondary: '#000',
+    green: '#43A854',
+    red: '#E34747',
+    lightGray: '#E8EFF1',
+    lightGray2: '#CBDDE2',
+    lightBlue: '#8792F1'
+}
+
+export default theme;
