14500f4ab1528f9f9b9d50857a4b054addb91148
jmendozacr <mendoza077@gmail.com>
2021-04-26 20:55:35 -0600

    adding helmet library to change favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from "react-helmet";
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader';
@@ -10,6 +11,8 @@ import Register from './pages/Register';
 import EditExpenses from './pages/EditExpenses';
 import ExpensesByCategory from './pages/ExpensesByCategory';
 import ExpensesList from './pages/ExpensesList';
+import favicon from './images/logo.png'
+import Background from './components/commons/Background';
 
 WebFont.load({
     // Work+Sans:wght@400;500;700
@@ -20,30 +23,36 @@ WebFont.load({
 
 export const Index = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Container>
-                    <Route path="/login">
-                        <Login/>
-                    </Route>
-                    <Route path="/register">
-                        <Register/>
-                    </Route>
-                    <Route path="/edit/:id">
-                        <EditExpenses/>
-                    </Route>
-                    <Route path="/expenses-by-category">
-                        <ExpensesByCategory/>
-                    </Route>
-                    <Route path="/expenses-list">
-                        <ExpensesList/>
-                    </Route>
-                    <Route exact={true} path="/">
-                        <App />
-                    </Route>   
-                </Container>
-            </Switch>
-        </BrowserRouter>
+        <>
+            <Helmet>
+                <link rel="shortcut icon" href={favicon} type="image/x-icon"/>
+            </Helmet>
+            <BrowserRouter>
+                <Switch>
+                    <Container>
+                        <Route path="/login">
+                            <Login/>
+                        </Route>
+                        <Route path="/register">
+                            <Register/>
+                        </Route>
+                        <Route path="/edit/:id">
+                            <EditExpenses/>
+                        </Route>
+                        <Route path="/expenses-by-category">
+                            <ExpensesByCategory/>
+                        </Route>
+                        <Route path="/expenses-list">
+                            <ExpensesList/>
+                        </Route>
+                        <Route exact={true} path="/">
+                            <App />
+                        </Route>   
+                    </Container>
+                </Switch>
+            </BrowserRouter>
+            <Background/>
+        </>
     );
 }
 
