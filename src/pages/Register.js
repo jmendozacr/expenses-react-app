02111bd99ccf3c9489d9191aa801c6f6e758ee77
Josue Mendoza <josuem@thehangar.cr>
2021-04-27 13:44:15 -0600

    adding register form page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import  { Header, HeaderContainer, Title } from './../components/commons/Header';
+import Button from './../components/commons/Button';
+import { Form, Input, ButtonContainer } from './../components/commons/ElementsForm';
+import { ReactComponent as SvgLogin } from './../images/registro.svg'
+import styled from 'styled-components';
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 6.25rem; /* 100px */
+    margin-bottom: 1.25rem; /* 20px */
+`;
 
 const Register = () => {
     return (
-        <div>
-            Register page.
-        </div>
+        <>
+             <Helmet>
+                <title>Register</title>
+            </Helmet>
+            <Header>
+                <HeaderContainer>
+                    <Title>
+                        Expenses by category page
+                    </Title>
+                    <div>
+                        <Button to="/login">Login</Button>
+                    </div>
+                </HeaderContainer>
+            </Header>
+            <Form>
+                <Svg/>
+                <Input 
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                />
+                <Input 
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                />
+                <Input 
+                    type="password"
+                    name="password2"
+                    placeholder="repeat password"
+                />
+                <ButtonContainer>
+                    <Button type="submit" as="button" primary>Register</Button>
+                </ButtonContainer>
+            </Form>
+        </>
     );
 }
 
