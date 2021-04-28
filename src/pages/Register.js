c5a3c33ddbf7f53d0fd1a2d8968cf4f03dbeaae4
Josue Mendoza <josuem@thehangar.cr>
2021-04-28 10:30:34 -0600

    adding handleChange and handleSubmit functions with firebase integration

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import styled from 'styled-components';
 import { Helmet } from 'react-helmet';
 import  { Header, HeaderContainer, Title } from './../components/commons/Header';
 import Button from './../components/commons/Button';
 import { Form, Input, ButtonContainer } from './../components/commons/ElementsForm';
 import { ReactComponent as SvgLogin } from './../images/registro.svg';
+import { auth } from '../firebase/firebaseConfig';
 
 const Svg = styled(SvgLogin)`
     width: 100%;
@@ -13,6 +15,55 @@ const Svg = styled(SvgLogin)`
 `;
 
 const Register = () => {
+    const history = useHistory();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    const handleChange = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmail(e.target.value);
+                break;
+            case 'password':
+                setPassword(e.target.value);
+                break;
+            case 'password2':
+                setPassword2(e.target.value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const exReg = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+        if( !exReg.test(email) ) {
+            console.log("error");
+            return;
+        }
+        if(email === '' || password === '' || password2 === '') {
+            console.log("there is a empty space");
+            return;
+        }
+        if(password !== password2) {
+            console.log("the password is not the same");
+            return;
+        }
+
+        try {
+            await auth.createUserWithEmailAndPassword(email, password);
+            console.log("user was registered");
+            history.push('/');
+        } catch (error) {
+            let message = error.message;
+
+            return console.log(message);
+        }
+    }
+
     return (
         <>
              <Helmet>
@@ -28,22 +79,30 @@ const Register = () => {
                     </div>
                 </HeaderContainer>
             </Header>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Svg/>
                 <Input 
                     type="email"
                     name="email"
                     placeholder="email"
+                    value={email}
+                    onChange={handleChange}
                 />
                 <Input 
                     type="password"
                     name="password"
                     placeholder="password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={handleChange}
                 />
                 <Input 
                     type="password"
                     name="password2"
+                    autoComplete="off"
                     placeholder="repeat password"
+                    value={password2}
+                    onChange={handleChange}
                 />
                 <ButtonContainer>
                     <Button type="submit" as="button" primary>Register</Button>
