60a1bc5877820a5f42784474fe18de978f3cf22f
jmendozacr <mendoza077@gmail.com>
2021-05-02 17:39:18 -0600

    category static options added

---
+++ src/utils/staticData.js
@@ -0,0 +1,12 @@
+const categories = [
+    {id: 'food', text: 'Food'},
+    {id: 'bills and payments', text: 'Bills and Payments'},
+    {id: 'home', text: 'Home'},
+    {id: 'transport', text: 'Transport'},
+    {id: 'clothing', text: 'Clothing'},
+    {id: 'salud e higiene', text: 'Health and Hygiene'},
+    {id: 'purchases', text: 'Purchases'},
+    {id: 'fun', text: 'Fun'}
+];
+
+export { categories };
