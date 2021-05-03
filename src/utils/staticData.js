cd35d3cb846e1267c0f16ee77018814d218b610b
jmendozacr <mendoza077@gmail.com>
2021-05-02 18:03:17 -0600

    update names

--- src/utils/staticData.js
+++ src/utils/staticData.js
@@ -1,10 +1,10 @@
 const categories = [
     {id: 'food', text: 'Food'},
-    {id: 'bills and payments', text: 'Bills and Payments'},
+    {id: 'payments', text: 'Bills and Payments'},
     {id: 'home', text: 'Home'},
     {id: 'transport', text: 'Transport'},
     {id: 'clothing', text: 'Clothing'},
-    {id: 'salud e higiene', text: 'Health and Hygiene'},
+    {id: 'health', text: 'Health and Hygiene'},
     {id: 'purchases', text: 'Purchases'},
     {id: 'fun', text: 'Fun'}
 ];
