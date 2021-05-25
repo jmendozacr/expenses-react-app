0daae2dd4a43fba9f2bca1a59c990ce1c46651d3
jmendozacr <mendoza077@gmail.com>
2021-05-24 20:42:30 -0600

    importing and building expenses list component

--- src/pages/ExpensesList.js
+++ src/pages/ExpensesList.js
@@ -4,6 +4,27 @@ import BackButton from '../components/commons/BackButton';
 import TotalBar from '../components/TotalBar';
 import useGetExpenses from '../hooks/useGetExpenses';
 import  { Header, Title } from './../components/commons/Header';
+import { 	List,
+            ElementList,
+            CategoryList,
+            ElementCategoryList,
+            Category,
+            Description,
+            Value,
+            Date,
+            ButtonContainer,
+            ButtonAction,
+            ButtonLoadMore,
+            ContainerCentralButton,
+            ContainerSubtitle,
+            Subtitle
+} from '../components/ElementList';
+import CategoryIcon from '../components/commons/CategoryIcons';
+import convertToCurrency from '../utils/convertToCurrency';
+import { ReactComponent as EditIcon } from './../images/editar.svg';
+import { ReactComponent as DeleteIcon } from './../images/borrar.svg';
+import { Link } from 'react-router-dom';
+import Button from './../components/commons/Button';
 
 const ExpensesList = () => {
     const [expenses] = useGetExpenses();
@@ -21,6 +42,56 @@ const ExpensesList = () => {
                     Expenses page
                 </Title>
             </Header>
+
+            <List>
+                {
+                    expenses.map((item, i) => {
+                        return(
+                            <ElementList key={i}>
+                                <Category>
+                                    <CategoryIcon name={item.category.toLowerCase()}/>
+                                    {item.category}
+                                </Category>
+
+                                <Description>
+                                    {item.description}
+                                </Description>
+
+                                <Value>
+                                    {convertToCurrency(item.quantity)}
+                                </Value>
+
+                                <ButtonContainer>
+                                    <ButtonAction as={Link} to={`/edit/${item.id}`}>
+                                        <EditIcon/>
+                                    </ButtonAction>
+                                    <ButtonAction>
+                                        <DeleteIcon/>
+                                    </ButtonAction>
+                                </ButtonContainer>
+                            </ElementList>
+                        )
+                    })
+                }
+
+                <ContainerCentralButton>
+                    <ButtonLoadMore>
+                        Load More
+                    </ButtonLoadMore>
+                </ContainerCentralButton>
+
+                {
+                    expenses.length === 0 &&
+                        <ContainerSubtitle>
+                            <Subtitle>
+                                There is no expense to show
+                            </Subtitle>
+                            <Button as={Link} to='/'>
+                                Add expense
+                            </Button>
+                        </ContainerSubtitle>
+                }
+            </List>
             <TotalBar/>
         </>
     );
