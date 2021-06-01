97d537b7a81d8b365d286f856856aac52724788c
Josue Mendoza <josuem@thehangar.cr>
2021-06-01 11:02:17 -0600

    adding getMoreExpenses function to load more

--- src/hooks/useGetExpenses.js
+++ src/hooks/useGetExpenses.js
@@ -5,6 +5,32 @@ import { db } from './../firebase/firebaseConfig';
 const useGetExpenses = () => {
     const { user } = useAuth();
     const [expenses, setExpenses] = useState([]);
+    const [lastExpense, setLastExense] = useState(null);
+    const [loadMoreValidation, setLoadMoreValidation] = useState(false);
+
+    const getMoreExpenses = () => {
+
+        db.collection('expenses')
+        .where('uidUser', '==', user.uid)
+        .orderBy('date', 'desc')
+        .limit(10)
+        .startAfter(lastExpense)
+        .onSnapshot((snapshot) => {
+            let newExpenses = [];
+
+            if(snapshot.docs.length > 0) {
+                setLastExense(snapshot.docs[snapshot.docs.length - 1]);
+
+                newExpenses = snapshot.docs.map(expense => {
+                    return {...expense.data(), id: expense.id}
+                });
+
+                setExpenses([...expenses, ...newExpenses]);
+            } else {
+                setLoadMoreValidation(false);
+            }
+        })
+    }
 
     useEffect(() => {
         const unsuscribe = db.collection('expenses')
@@ -12,6 +38,13 @@ const useGetExpenses = () => {
             .orderBy('date', 'desc')
             .limit(10)
             .onSnapshot((snapshot) => {
+                if(snapshot.docs.length > 0 ) {
+                    setLastExense(snapshot.docs[snapshot.docs.length - 1]);
+                    setLoadMoreValidation(true);
+                } else {
+                    setLoadMoreValidation(false);
+                }
+
                 setExpenses(snapshot.docs.map(expense => {
                     return {...expense.data(), id: expense.id}
                 }));
@@ -20,7 +53,7 @@ const useGetExpenses = () => {
         return unsuscribe;
     }, [user])
 
-    return [expenses];
+    return [expenses, getMoreExpenses, loadMoreValidation];
 }
 
 export default useGetExpenses;
