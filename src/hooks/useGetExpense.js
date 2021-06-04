32d69c5c95c3575d353e39cb6896a6777ca0b36e
jmendozacr <mendoza077@gmail.com>
2021-06-03 20:39:24 -0600

    adding useGetExpense hook

---
+++ src/hooks/useGetExpense.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { db } from "../firebase/firebaseConfig";
+
+const useGetExpense = (id) => {
+    const [expense, setExpense] = useState('');
+    const history = useHistory();
+
+    useEffect(() => {
+        db.collection('expenses').doc(id).get()
+        .then((doc) => {
+            if(doc.exists) {
+                setExpense(doc);
+            } else {
+                history.push('/expenses');
+            }
+        })
+    }, [history, id])
+
+
+    return [expense];
+}
+
+export default useGetExpense;
--- src/pages/EditExpenses.js
+++ src/pages/EditExpenses.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { useParams } from 'react-router';
+import BackButton from '../components/commons/BackButton';
+import ExpenseForm from '../components/ExpenseForm';
+import TotalBar from '../components/TotalBar';
+import useGetExpense from '../hooks/useGetExpense';
+import  { Header, Title } from './../components/commons/Header';
 
 const EditExpenses = () => {
+    const { id } = useParams();
+    // console.log('id', id);
+    const [expense] = useGetExpense(id);
+    console.log('expense', expense);
+
     return (
-        <div>
-            Edit Expenses page.
-        </div>
+        <>
+            <Helmet>
+                <title>Edit Expense</title>
+            </Helmet>
+
+            <Header>
+                <BackButton/>
+                <Title>
+                    Edit Expense
+                </Title>
+            </Header>
+            <ExpenseForm/>
+            <TotalBar/>
+        </>
     );
 }
 
