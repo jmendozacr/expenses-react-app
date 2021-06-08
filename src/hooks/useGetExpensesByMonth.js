5bbfd673d7f31e2b309a86c45525dc4bec87b7a4
jmendozacr <mendoza077@gmail.com>
2021-06-07 21:30:00 -0600

    creating custom hook useGetExpensesByMonth

---
+++ src/hooks/useGetExpensesByMonth.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { db } from "../firebase/firebaseConfig";
+import { startOfMonth, endOfMonth, getUnixTime } from 'date-fns';
+import { useAuth } from "../context/authContext";
+
+
+const useGetExpensesByMonth = () => {
+    const [expenses, setExpenses] = useState([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        const startOfMonthData = getUnixTime(startOfMonth(new Date()));
+        const endOfMonthData = getUnixTime(endOfMonth(new Date()));
+
+        if(user) {
+            const unsuscribe = db.collection("expenses")
+                .orderBy('date', 'desc')
+                .where('date', '>=', startOfMonthData)
+                .where('date', '<=', endOfMonthData)
+                .where('uidUser', '==', user.uid)
+                .onSnapshot((snapshot) => {
+                    setExpenses(snapshot.docs.map(expense => {
+                        return {...expense.data(), id: expense.id}
+                    }));
+                });
+
+            return unsuscribe;
+        }
+
+    }, [user]);
+
+    return [expenses];
+}
+
+export default useGetExpensesByMonth;
