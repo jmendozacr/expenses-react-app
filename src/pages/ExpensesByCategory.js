f253f2246c52a5ebea5b0b0c5de70ce74185d825
jmendozacr <mendoza077@gmail.com>
2021-06-12 09:44:21 -0600

    importing CategoryList and mapping expenses by category

--- src/pages/ExpensesByCategory.js
+++ src/pages/ExpensesByCategory.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import BackButton from '../components/commons/BackButton';
+import CategoryIcon from '../components/commons/CategoryIcons';
 import TotalBar from '../components/TotalBar';
 import useGetExpensesByMonthAndCategories from '../hooks/useGetExpensesByMonthAndCategories';
+import convertToCurrency from '../utils/convertToCurrency';
 import  { Header, Title } from './../components/commons/Header';
+import { CategoryList, ElementCategoryList, Category, Value } from './../components/ElementList';
 
 const ExpensesByCategory = () => {
-    const expenses = useGetExpensesByMonthAndCategories();
-    console.log('expenses', expenses);
+    const expensesByCategory = useGetExpensesByMonthAndCategories();
 
     return (
         <>
@@ -21,6 +23,22 @@ const ExpensesByCategory = () => {
                     Expenses by category page
                 </Title>
             </Header>
+
+            <CategoryList>
+                {
+                    expensesByCategory.map((item, i) => {
+                        return(
+                            <ElementCategoryList key={i}>
+                                <Category>
+                                    <CategoryIcon name={item.category}/>
+                                    {item.category}
+                                </Category>
+                                <Value>{convertToCurrency(item.quantity)}</Value>
+                            </ElementCategoryList>
+                        )
+                    })
+                }
+            </CategoryList>
             <TotalBar/>
         </>
     );
